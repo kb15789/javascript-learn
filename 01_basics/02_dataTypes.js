ffdcf40f37806d067c921cc906831e76fa8a4548
kb15789 <kittubose2010@gmail.com>
2024-09-14 14:42:45 +0530

    First Day: Learnt aboyt dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all JS code with newer version
+
+// alert( 3+3) // we are using nodejs and not browser, so it throws error
+
+let name = "kittu" //string
+let age = 18 //numeric
+let isloggedIn = false //boolean
+
+/*ecma -> since different browsers were creating 
+diff methodologies for their js, ecma is an organizaton
+that standardizeed the approac */
+
+
+console.log(typeof age)
+
+
