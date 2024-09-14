f17f6966b27055f054871309aa59c19077565b10
kb15789 <kittubose2010@gmail.com>
2024-09-14 14:59:38 +0530

    First Day: Learnt about data conversions

---
+++ 01_basics/03_conversionOps.js
@@ -0,0 +1,16 @@
+let score = "33"
+
+console.log(typeof score);
+
+//convert score to number
+
+let valinNum = Number(score)
+console.log(typeof valinNum)
+//"33" => 33
+// "33abc" => NaN
+//underfine => NaN
+//true =>1 and false => 0
+
+let name_val = ""
+let boolean_name_val = Boolean(name_val)
+console.log( boolean_name_val)
