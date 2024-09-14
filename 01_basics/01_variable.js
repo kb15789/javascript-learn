f0ab9d76a5b5f414f3be55bb7ea102c9c078192b
kb15789 <kittubose2010@gmail.com>
2024-09-14 14:25:39 +0530

    First Day: Learnt about let and const

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountId=  144553 
+let email = "kb@google.com"
+var acountPassword = "1222"
+accountCity = "Pune"
+
+//const value cannot be changed
+
+email = "hc@hc.com"
+acountPassword = "122222"
+accountCity = 'Kolkata'
+
+// do not use var and only use let . Var has a blocked scope issue
+
+let accountState;
+console.table([email, accountId, acountPassword, accountCity, accountState]);
+
+
+
+
+
